dac86144346a4c2aed71dbac6d310cf030a12688
Emerson-Okopnik <emer00k@gmail.com>
2025-07-30 14:16:29 -0300

    Ajustes vite

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import vue from "@vitejs/plugin-vue"
 export default defineConfig({
   plugins: [vue()],
   server: {
-    //host: '192.168.200.128',
     port: 5173, // opcional
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "http://relatorios.rudniksolucoes.com.br:3001",
         changeOrigin: true,
       },
     },
